5599f3944b3a7fa4432e719efb05fd19b99c4407
Stormtrooper66 <redstoner66@gmail.com>
2017-07-21 17:40:55 -0400

    Update RenderRobotChicken.java

--- java/strormtrooper/newmods/render/RenderRobotChicken.java
+++ java/strormtrooper/newmods/render/RenderRobotChicken.java
@@ -1,29 +1,39 @@
-package strormtrooper.newmods.render;
+package strormtrooper.newmods.Render;
 
 import strormtrooper.newmods.Entity.EntityRobotChicken;
+import strormtrooper.newmods.Entity.EntityRobotChickenMob;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.util.ResourceLocation;
 
-public class RenderRobotChicken extends RenderLiving{
+@SideOnly(Side.CLIENT)
+public class RenderRobotChickenMob extends RenderLiving
+{
+    private static final ResourceLocation mobTextures = new ResourceLocation("textures/entity/snowman.png");
+    private static final String __OBFID = "CL_00000984";
 
-	private static final ResourceLocation doeTexture = new ResourceLocation("frst:" + ":textures/reg_models/RobotChicken.png");
+    public RenderRobotChickenMob(ModelBase p_i1253_1_, float p_i1253_2_)
+    {
+        super(p_i1253_1_, p_i1253_2_);
+    }
 
-		 public RenderRobotChicken(ModelBase par1ModelBase, float par2) 
-		{
-			 super(par1ModelBase, par2);
-		}
-		 
-		protected ResourceLocation getEntityTexture(EntityRobotChicken entity) 
-		{
-		return doeTexture;
-		}
-		
-		protected ResourceLocation getEntityTexture(Entity entity)
-		{
-			return this.getEntityTexture((EntityRobotChicken) entity);
-		}
+    /**
+     * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
+     */
+    protected ResourceLocation getEntityTexture(EntityRobotChickenMob p_110775_1_)
+    {
+        return mobTextures;
+    }
 
-
+    /**
+     * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
+     */
+    protected ResourceLocation getEntityTexture(Entity p_110775_1_)
+    {
+        return this.getEntityTexture((EntityRobotChickenMob)p_110775_1_);
+    }
 }
