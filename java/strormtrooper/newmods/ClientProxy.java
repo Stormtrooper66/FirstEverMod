d971ec02c8176eaf950777d2877c0c059757f45b
Stormtrooper66 <redstoner66@gmail.com>
2017-08-04 18:19:58 -0400

    Update ClientProxy.java

--- java/strormtrooper/newmods/ClientProxy.java
+++ java/strormtrooper/newmods/ClientProxy.java
@@ -2,13 +2,59 @@
 
 import net.minecraft.client.model.ModelChicken;
 import strormtrooper.newmods.Entity.EntityRobotChickenMob;
+import strormtrooper.newmods.Entity.EntityWalkerMob;
+import strormtrooper.newmods.Models.RobotChickenModel;
+import strormtrooper.newmods.Models.WalkerModel;
 import strormtrooper.newmods.Render.RenderRobotChickenMob;
+import strormtrooper.newmods.Render.RenderWalkerMob;
 import cpw.mods.fml.client.registry.RenderingRegistry;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 public class ClientProxy extends ServerProxy{
 	
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		// DEBUG
+        System.out.println("PreInit on Client side");
+        
+		// do common stuff
+		super.fmlLifeCycleEvent(event);
+
+        // do client-specific stuff
+        registerRenderThings();
+	}
+	@EventHandler
+	public void Init(FMLInitializationEvent event)
+	{
+		// DEBUG
+        System.out.println("Init on Client side");
+
+        // do common stuff
+		super.fmlLifeCycleEvent(event);
+
+		// do client-specific stuff
+	}
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		// DEBUG
+        System.out.println("PostInit on Client side");
+
+        // do common stuff
+		super.fmlLifeCycleEvent(event);
+
+		// do client-specific stuff
+	}
+	
 	public void registerRenderThings(){
-		
-		RenderingRegistry.registerEntityRenderingHandler(EntityRobotChickenMob.class,new RenderRobotChickenMob(new ModelChicken(), 0));
-}
+		System.out.println("Rendering Robot Chicken");
+		RenderingRegistry.registerEntityRenderingHandler(EntityRobotChickenMob.class,new RenderRobotChickenMob(new RobotChickenModel(), 0.0F));
+		System.out.println("Rendering Walker");
+		RenderingRegistry.registerEntityRenderingHandler(EntityWalkerMob.class, new RenderWalkerMob(new WalkerModel(),0.0F));
+	}
 }
