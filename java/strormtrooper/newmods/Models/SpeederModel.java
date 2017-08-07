9ca457c67f24054c4d159342c6aa4ef8f7800163
Stormtrooper66 <redstoner66@gmail.com>
2017-08-07 10:38:53 -0400

    Update SpeederModel.java

--- java/strormtrooper/newmods/Models/SpeederModel.java
+++ java/strormtrooper/newmods/Models/SpeederModel.java
@@ -23,6 +23,16 @@ public class SpeederModel extends ModelBase
         protected double distanceMovedTotal = 0.0D;
         
         public SpeederModel(){
-         
+               Head = new ModelRenderer(this,0,0);
+               Head.addbox(0F,0F,0F,5,5,32);
+               Head.setRotationPoint(-5F,(24F- 24F),-4F);
+               Head.setTextureSize(TextureWidth,TextureHeight);
          
          }
+         public void render(Entity p_78088_1_, float p_78088_2_, float p_78088_3_, float p_78088_4_, float p_78088_5_, float p_78088_6_, float p_78088_7_){
+		this.setRotationAngles(p_78088_2_, p_78088_3_, p_78088_4_, p_78088_5_, p_78088_6_, p_78088_7_, p_78088_1_);
+
+		Head.render(p_78088_7_);
+                }
+        
+}
