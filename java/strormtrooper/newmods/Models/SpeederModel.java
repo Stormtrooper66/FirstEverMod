64ac4b5150c9a92e942b1e626989be9f601b278e
Stormtrooper66 <redstoner66@gmail.com>
2017-08-07 10:45:01 -0400

    Update SpeederModel.java

--- java/strormtrooper/newmods/Models/SpeederModel.java
+++ java/strormtrooper/newmods/Models/SpeederModel.java
@@ -11,7 +11,7 @@ import net.minecraft.util.MathHelper;
 
 public class SpeederModel extends ModelBase
 {
-        public ModelRenderer Head;
+        public ModelRenderer Body;
         
         
         
@@ -23,16 +23,17 @@ public class SpeederModel extends ModelBase
         protected double distanceMovedTotal = 0.0D;
         
         public SpeederModel(){
-               Head = new ModelRenderer(this,0,0);
-               Head.addbox(0F,0F,0F,5,5,32);
-               Head.setRotationPoint(-5F,(24F- 24F),-4F);
-               Head.setTextureSize(TextureWidth,TextureHeight);
+               Body = new ModelRenderer(this,0,0);
+               Body.addbox(0F,0F,0F,5,5,32);
+               Body.setRotationPoint(-5F,(24F- 24F),-4F);
+               Body.setTextureSize(TextureWidth,TextureHeight);
+	       
          
          }
          public void render(Entity p_78088_1_, float p_78088_2_, float p_78088_3_, float p_78088_4_, float p_78088_5_, float p_78088_6_, float p_78088_7_){
 		this.setRotationAngles(p_78088_2_, p_78088_3_, p_78088_4_, p_78088_5_, p_78088_6_, p_78088_7_, p_78088_1_);
 
-		Head.render(p_78088_7_);
+		Body.render(p_78088_7_);
                 }
         
 }
