79fb37e5118f002ed145c04cebf735d350a89a59
Stormtrooper66 <redstoner66@gmail.com>
2017-08-07 09:53:28 -0400

    Update SpeederModel.java

--- java/strormtrooper/newmods/Models/SpeederModel.java
+++ java/strormtrooper/newmods/Models/SpeederModel.java
@@ -1,1 +1,15 @@
+package strormtrooper.newmods.Models;
 
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.MathHelper;
+
+
+
+public class SpeederModel extends ModelBase
+{
+public int TextureWidth = 64;
+public int TextureHeight = 32;
